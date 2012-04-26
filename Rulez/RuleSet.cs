407947d30ff6dfdf8fac3e906a4cf671578804ac
Armin Sander <armin@replicator.org>
2012-04-26 18:04:21 +0200

    experient (activate with COMPILEIT): compile the rules call

--- Rulez/RuleSet.cs
+++ Rulez/RuleSet.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 
 namespace Rulez
@@ -32,6 +34,12 @@ namespace Rulez
 				args[i] = resolver_(parameters[i].ParameterType);
 
 			addRule(() => method.Invoke(this, args));
+
+#if COMPILEIT
+			var parameterExpressions = args.Select(Expression.Constant);
+			var call = Expression.Call(Expression.Constant(this), method, parameterExpressions);
+			addRule(Expression.Lambda<Action>(call).Compile());
+#endif
 		}
 
 		public void Dispose()
