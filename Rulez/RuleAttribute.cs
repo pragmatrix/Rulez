e1708e08c3d05d675b5f96e4f7b46551946bc731
Armin Sander <armin@replicator.org>
2012-04-26 17:32:18 +0200

    implicitly add methods to the rule set when they are attribute with [Rule]

---
+++ Rulez/RuleAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Rulez
+{
+	/*
+		If a method is in a class derived from RuleSet and attributed 
+		with the [Rule] attribute, it is automatically added to the RuleSet.
+	*/
+
+	[AttributeUsage(AttributeTargets.Method)]
+	public sealed class RuleAttribute : Attribute
+	{
+	}
+}
--- Rulez/RuleSet.cs
+++ Rulez/RuleSet.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace Rulez
 {
 	public class RuleSet : IDisposable
 	{
 		readonly List<IDisposable> _rules = new List<IDisposable>();
+
+		public RuleSet()
+		{
+			var t = GetType();
+			var methods = t.GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+			foreach (var m in methods)
+			{
+				if (m.GetCustomAttributes(typeof (RuleAttribute), false).Length == 0)
+					continue;
+				if (m.GetParameters().Length != 0)
+					throw new Exception("Rules are now allowed to have parameters (yet).");
+				var mCopy = m;
+				addRule(() => mCopy.Invoke(this, null));
+			}
+		}
 
 		public void Dispose()
 		{
@@ -18,7 +34,7 @@ namespace Rulez
 			_rules.Clear();
 		}
 
-		public void add(Action rule)
+		public void addRule(Action rule)
 		{
 			_rules.Add(Rule.activate(rule));
 		}
